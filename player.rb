49538da8e5ed6a331e724e72f270a02dc117790b
knifeofdreams <judit.ordog-andrasi@prezi.com>
2015-09-26 14:39:45 +0200

    so random :D

--- player.rb
+++ player.rb
@@ -1,31 +1,38 @@
 
 class Player
 
-  VERSION = "randombot"
+  VERSION = "SOOOOrandombot"
 
   def bet_request(game_state)
-    first_high_card = is_high_card?(game_state, 0)
-    second_high_card = is_high_card?(game_state, 1)
-
-    bet = 0
-    if first_high_card or second_high_card
+    if face_card_in_hand?(game_state)
       bet = game_state['players'][game_state['in_action']]['stack']/6.floor
     else
-      bet = 100
+      bet = (100 * rand()).floor
     end
+
+    game_state['players'][game_state['in_action']]['stack']/6.floor
 
     if bet < minimum_call_bet(game_state)
       return minimum_call_bet(game_state)
     end
 
-    if rand() > 0.8
-      bet = 0
-    end
+    bet = 0 if random_fold?
 
     bet
   end
 
-  def is_high_card?(game_state, card)
+  def face_card_in_hand?(game_state)
+    first_high_card = is_face_card?(game_state, 0)
+    second_high_card = is_face_card?(game_state, 1)
+
+     return (first_high_card or second_high_card)
+  end
+
+  def random_fold?
+    rand() > 0.8
+  end
+
+  def is_face_card?(game_state, card)
     game_state['players'][game_state['in_action']]['hole_cards'][card]['rank'] == 'A' or game_state['players'][game_state['in_action']]['hole_cards'][card]['rank'] == 'Q' or game_state['players'][game_state['in_action']]['hole_cards'][card]['rank'] == 'K' or game_state['players'][game_state['in_action']]['hole_cards'][card]['rank'] == 'J'
   end
 
