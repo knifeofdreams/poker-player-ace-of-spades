80db436e308839ee62b5c6037be05c2a5f992bf0
knifeofdreams <judit.ordog-andrasi@prezi.com>
2015-09-26 13:33:35 +0200

    check my stack

--- player.rb
+++ player.rb
@@ -1,22 +1,21 @@
 
 class Player
 
-  VERSION = "fix integer"
+  VERSION = "check my stack"
 
   def bet_request(game_state)
-    first_high_card = high_card(game_state, 0)
-    second_high_card = high_card(game_state, 1)
+    first_high_card = is_high_card?(game_state, 0)
+    second_high_card = is_high_card?(game_state, 1)
 
     if first_high_card or second_high_card
-      # game_state['players'][game_state['in_action']]['stack']/6
-      120
+      game_state['players'][game_state['in_action']]['stack']/6.floor
     else
       100
     end
   end
 
-  def high_card(game_state, card)
-    game_state['players'][game_state['in_action']]['hole_cards'][card]['rank'] if game_state['players'][game_state['in_action']]['hole_cards'][card]['rank'] == 'A' or game_state['players'][game_state['in_action']]['hole_cards'][card]['rank'] == 'Q' or game_state['players'][game_state['in_action']]['hole_cards'][card]['rank'] == 'K' or game_state['players'][game_state['in_action']]['hole_cards'][card]['rank'] == 'J'
+  def is_high_card?(game_state, card)
+    game_state['players'][game_state['in_action']]['hole_cards'][card]['rank'] == 'A' or game_state['players'][game_state['in_action']]['hole_cards'][card]['rank'] == 'Q' or game_state['players'][game_state['in_action']]['hole_cards'][card]['rank'] == 'K' or game_state['players'][game_state['in_action']]['hole_cards'][card]['rank'] == 'J'
   end
 
   def showdown(game_state)
