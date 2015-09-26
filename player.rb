b0e6fbfd474da7cb2c906d323cc17b208472bad0
knifeofdreams <judit.ordog-andrasi@prezi.com>
2015-09-26 11:31:36 +0200

    high card

--- player.rb
+++ player.rb
@@ -1,14 +1,15 @@
 
 class Player
 
-  VERSION = "Ace!!!!"
+  VERSION = "High card"
+
+  (ONE_HIGH_CARD = game_state['players'][game_state['in_action']]['hole_cards'][0]['rank'] == 'A') or game_state['players'][game_state['in_action']]['hole_cards'][0]['rank'] == 'Q' or game_state['players'][game_state['in_action']]['hole_cards'][0]['rank'] == 'K' or game_state['players'][game_state['in_action']]['hole_cards'][0]['rank'] == 'J'
+  (OTHER_HIGH_CARD = game_state['players'][game_state['in_action']]['hole_cards'][1]['rank'] == 'A') or game_state['players'][game_state['in_action']]['hole_cards'][1]['rank'] == 'Q' or game_state['players'][game_state['in_action']]['hole_cards'][1]['rank'] == 'K' or game_state['players'][game_state['in_action']]['hole_cards'][1]['rank'] == 'J'
 
   def bet_request(game_state)
-    if game_state['players'][game_state['in_action']]['hole_cards'][0]['rank'] == 'A' or game_state['players'][game_state['in_action']]['hole_cards'][0]['rank'] == 'Q' or game_state['players'][game_state['in_action']]['hole_cards'][0]['rank'] == 'K' or game_state['players'][game_state['in_action']]['hole_cards'][0]['rank'] == 'J'
-      return 150
+    if ONE_HIGH_CARD or OTHER_HIGH_CARD
+      return 200
     end
-
-
     100
   end
 
